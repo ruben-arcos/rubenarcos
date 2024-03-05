c01578905f0ff1797094201ec299f28540ee0c8e
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-05 03:00:12 -0600

    finish styling the image of the intro section. began working on the text in the intro section. modified the eslint file.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Intro from "@/components/Intro";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="">
+  return <main className="flex flex-col items-center px-4">
     <Intro />
   </main>;
 }
