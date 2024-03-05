9110c5df29944ac9cf665c4c19b472798bfcc1d3
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-04 23:42:59 -0600

    started working with the theme switch feature.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import ThemeSwitch from "@/components/ThemeSwitch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
         <Header />
         {children}
+        <ThemeSwitch />
       </body>
     </html>
   );
---
+++ components/ThemeSwitch.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BsSun } from 'react-icons/bs'
+
+export default function ThemeSwitch() {
+  return <button className=" fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full items-center justify-center hover:scale-[1.15] active:scale-105 active:scale transition-all">
+    <BsSun />
+  </button>
+}
