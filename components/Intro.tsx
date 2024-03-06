7d91bca07304475f6f650dccf04643df97b61ae2
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-05 19:41:59 -0600

    organized the classes in the buttons

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -53,9 +53,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Ruben.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="font-bold">full-stack developer</span> with a passion for building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.p>
 
