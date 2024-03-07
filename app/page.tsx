be6fdd3885efafab5d29a2f4801f7b910756377a
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-06 20:29:43 -0600

    modified the p section of intro to and h1. created the about section and started working on styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Intro from "@/components/Intro";
 import SectionDivider from "@/components/SectionDivider";
 import Image from "next/image";
@@ -6,5 +7,6 @@ export default function Home() {
   return <main className="flex flex-col items-center px-4">
     <Intro />
     <SectionDivider />
+    <About />
   </main>;
 }
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -47,7 +47,7 @@ export default function Intro() {
         </div>
       </div>
 
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -55,7 +55,7 @@ export default function Intro() {
         <span className="font-bold">Hello, I'm Ruben.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with a passion for building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className="flex flex-co; sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
