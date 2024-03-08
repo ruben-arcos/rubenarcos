51c7f6ed09caf324438c953e3e5a8afa5379e024
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-08 00:15:12 -0600

    added styling in sectionHeading file for the h2 tags.

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -8,6 +8,6 @@ export default function SectionHeading({
     children
 } : SectionHeadingProps) {
   return (
-    <h2 className="capitalize text-3xl font-medium mb-8">{children}</h2>
+    <h2 className="capitalize text-3xl font-medium mb-8 text-center">{children}</h2>
     )
 }
