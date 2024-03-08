20714a4f0dfd5b81edfad90e4d6daa9a66458346
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-07 22:26:55 -0600

    added projects to pages and imported it. Added title to  section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import About from "@/components/About";
 import Intro from "@/components/Intro";
+import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="flex flex-col items-center px-4">
-    <Intro />
-    <SectionDivider />
-    <About />
-  </main>;
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+    </main>
+  );
 }
