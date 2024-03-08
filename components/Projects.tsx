0edee572685b69235b5759814080a27b4015b4ea
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-07 22:54:09 -0600

    created function to display title of project, description, and tags.

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import SectionHeading from "./SectionHeading";
+import { projectsData } from "@/lib/data";
 
 export default function Projects() {
   return (
     <section>
-        <SectionHeading>my projects</SectionHeading>
+      <SectionHeading>my projects</SectionHeading>
+
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
     </section>
-  )
+  );
+}
+
+type ProjectProps = (typeof projectsData)[number];
+
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+  return (
+    <section>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        {tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+    </section>
+  );
 }
