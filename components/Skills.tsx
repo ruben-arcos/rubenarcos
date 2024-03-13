a7cad4b26b6afaf722780adbb93b7fbe2e74befd
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-12 23:41:59 -0500

    styled the ul

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,12 +6,10 @@ export default function Skills() {
   return (
     <section className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
       <SectionHeading>my skills</SectionHeading>
-      <ul>
-        {
-            skillsData.map((skill, index) => (
-                <li key={index}>{skill}</li>
-            ))
-        }
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
       </ul>
     </section>
   );
