fec4ec0ebf279d6812747710322a7a990e7a2aff
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-12 22:42:27 -0500

    created added a section with a ul, li to skills file. added imported the skills file to the page file.

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import SectionHeading from "./SectionHeading";
+import { skillsData } from "@/lib/data";
 
 export default function Skills() {
   return (
     <section>
+      <SectionHeading>my skills</SectionHeading>
+      <ul>
+        {
+            skillsData.map((skill, index) => (
+                <li key={index}>{skill}</li>
+            ))
+        }
+      </ul>
     </section>
   );
 }
