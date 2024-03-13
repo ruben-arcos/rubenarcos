350d6412e0cef6f085cc2f2b65222007e9e31c5d
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-13 01:13:58 -0500

    started working on the experience section. created the experience.tsx file. Installed the vertical timeline component package

---
+++ components/Experience.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SectionHeading from "./SectionHeading";
+
+export default function Experience() {
+  return (
+    <section id="experience">
+      <SectionHeading>my experience</SectionHeading>
+    </section>
+  );
+}
