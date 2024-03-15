157e09fa5bc42c3a1a8548dac58e1793fbd2e73c
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-14 23:47:20 -0500

    started styling the experience section.

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,13 +1,35 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./SectionHeading";
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
 
 export default function Experience() {
   return (
-    <section id="experience">
+    <section id="experience" className="scroll-mt-28">
       <SectionHeading>my experience</SectionHeading>
+      <VerticalTimeline>
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              visible={true}
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+              }}
+            >
+              <h3>{item.title}</h3>
+              <p>{item.location}</p>
+              <p>{item.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </section>
   );
 }
