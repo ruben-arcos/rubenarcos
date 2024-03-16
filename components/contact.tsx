2cd6ddb76ffb4a8ffe55e5b2f0ece3587f719535
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-15 23:40:24 -0500

    wrapped email in a <a> tag

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
     <section id="contact">
       <SectionHeading>contact me</SectionHeading>
-      <p>please contact me directly at ruben.arcos06@gmail.com or through this form</p>
+      <p>please contact me directly at <a href="mailto:ruben.arcos06@gmail.com">ruben.arcos06@gmail.com</a> or through this form</p>
     </section>
   );
 }
