c697dc5c66010e32adecac51abffffc5ba08dca3
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-15 23:36:34 -0500

    added paragraph to the contact section which includes my email.

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,6 +5,7 @@ export default function Contact() {
   return (
     <section id="contact">
       <SectionHeading>contact me</SectionHeading>
+      <p>please contact me directly at ruben.arcos06@gmail.com or through this form</p>
     </section>
   );
 }
