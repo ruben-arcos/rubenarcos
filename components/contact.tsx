f7751cb03719bc1ddbc010b7d9cb4a8f1c7dd326
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-16 23:44:19 -0500

    created the form section with the input boxes, button, and icon for submit

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import SectionHeading from "./SectionHeading";
+import { FaPaperPlane } from "react-icons/fa";
 
 export default function Contact() {
   return (
     <section id="contact">
       <SectionHeading>contact me</SectionHeading>
-      <p>please contact me directly at <a href="mailto:ruben.arcos06@gmail.com">ruben.arcos06@gmail.com</a> or through this form</p>
+      <p>
+        please contact me directly at{" "}
+        <a href="mailto:ruben.arcos06@gmail.com">ruben.arcos06@gmail.com</a> or
+        through this form
+      </p>
+
+      <form>
+        <input type="email" />
+        <textarea />
+        <button type="submit">submit <FaPaperPlane /></button>
+      </form>
     </section>
   );
 }
