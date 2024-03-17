354602f32c116b2e76e982ccca1991ce2c04111c
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-17 02:05:01 -0500

    added the useInView hook into files experience and contact. added more styles to boxes and submit button in the contact file.

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -9,10 +9,13 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { inView } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Experience() {
+  const { ref } = useSectionInView("Experience");
+
   return (
-    <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
+    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>my experience</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,13 +1,32 @@
+"use client";
 
 import React from "react";
 import SectionHeading from "./SectionHeading";
 import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Contact() {
+    const { ref } = useSectionInView("Contact");
+
   return (
-    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)] ">
+    <motion.section
+      id="contact"
+      ref={ref}
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+    >
       <SectionHeading>contact me</SectionHeading>
-      <p className="text-gray-700">
+
+      <p className="text-gray-700 -mt-6">
         please contact me directly at{" "}
         <a className="underline" href="mailto:ruben.arcos06@gmail.com">
           ruben.arcos06@gmail.com
@@ -17,14 +36,27 @@ export default function Contact() {
 
       <form className="mt-10 flex flex-col">
         <input
-          className="h-14 rounded-lg borderBlack"
+          className="h-14 px-4 rounded-lg borderBlack"
           type="email"
+          placeholder="Your email"
         />
-        <textarea className="h-52 my-3 rounded-lg borderBlack p-4" />
-        <button type="submit" className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all">
-          submit <FaPaperPlane />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+          placeholder="Your message"
+        />
+
+        <button
+          type="submit"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
+        >
+          submit{" "}
+          <FaPaperPlane
+            className="text-xs opacity-70 transition-all 
+            group-hover:translate-x-1 
+            group-hover:-translate-y-1"
+          />{" "}
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 }
