b1f684e45405fac5b7be94db5b407d8a3cedfdc1
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-17 23:06:54 -0500

    added resend package and on sendEmail file modified.

--- actions/SendEmail.ts
+++ actions/SendEmail.ts
@@ -1,7 +1,18 @@
-'use server'
+"use server";
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const SendEmail = async (formData: FormData) => {
-    console.log('running on server')
-    console.log(formData.get('senderEmail'));
-    console.log(formData.get('message'));
-  }
+  console.log("running on server");
+  console.log(formData.get("senderEmail"));
+  console.log(formData.get("message"));
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "ruben.arcos06@gmail.com",
+    subject: "Message from contact form",
+    text: "Hello world!",
+  });
+};
