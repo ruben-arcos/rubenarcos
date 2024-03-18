415203c33113c0944aecb02aa87671f0d21b1750
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-18 04:07:14 -0500

    added more styles to contact section for dark mode

--- components/contact.tsx
+++ components/contact.tsx
@@ -31,7 +31,7 @@ export default function Contact() {
     >
       <SectionHeading>contact me</SectionHeading>
 
-      <p className="text-gray-700 -mt-6">
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
         please contact me directly at{" "}
         <a className="underline" href="mailto:ruben.arcos06@gmail.com">
           ruben.arcos06@gmail.com
@@ -40,7 +40,7 @@ export default function Contact() {
       </p>
 
       <form
-        className="mt-10 flex flex-col"
+        className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
           const { data, error } = await SendEmail(formData);
 
@@ -52,7 +52,7 @@ export default function Contact() {
         }}
       >
         <input
-          className="h-14 px-4 rounded-lg borderBlack"
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="senderEmail"
           type="email"
           placeholder="Your email"
@@ -60,7 +60,7 @@ export default function Contact() {
           maxLength={500}
         />
         <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4"
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="message"
           placeholder="Your message"
           required
