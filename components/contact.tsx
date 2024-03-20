4879d8483d0a4724a8bcddf5fd33b9c67b7bc07e
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-20 01:42:12 -0500

    modified project

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import React from "react";
-import SectionHeading from "./section-heading";
+import SectionHeading from "./SectionHeading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import { sendEmail } from "@/actions/sendEmail";
-import SubmitBtn from "./submit-btn";
+import { SendEmail } from "@/actions/SendEmail";
+import SubmitBtn from "./SubmitBtn";
 import toast from "react-hot-toast";
 
 export default function Contact() {
@@ -29,26 +29,25 @@ export default function Contact() {
         once: true,
       }}
     >
-      <SectionHeading>Contact me</SectionHeading>
+      <SectionHeading>contact me</SectionHeading>
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Please contact me directly at{" "}
+        please contact me directly at{" "}
         <a className="underline" href="mailto:ruben.arcos06@gmail.com">
-        ruben.arcos06@gmail.com
+          ruben.arcos06@gmail.com
         </a>{" "}
-        or through this form.
+        or through this form
       </p>
 
       <form
         className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
-          const { data, error } = await sendEmail(formData);
+          const { data, error } = await SendEmail(formData);
 
           if (error) {
-            toast.error(error);
+            toast.error(error)
             return;
           }
-
           toast.success("Email sent successfully!");
         }}
       >
@@ -56,9 +55,9 @@ export default function Contact() {
           className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="senderEmail"
           type="email"
+          placeholder="Your email"
           required
           maxLength={500}
-          placeholder="Your email"
         />
         <textarea
           className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
