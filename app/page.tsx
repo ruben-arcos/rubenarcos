43d9ee6c66c304b05892f2e959841017ac3aa161
Ruben Arcos <ruben.arcos06@gmail.com>
2024-03-20 23:53:09 -0500

    modified the page contact import

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import About from "@/components/About";
-import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
 import Intro from "@/components/Intro";
 import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
 import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
